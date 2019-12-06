0030d04d31a965b21a7269cbaaeb9e8924e1c259
霍振鹏 <huozhenpengdyx@gmail.com>
2019-12-06 12:26:13 +0800

    根据官方例子改了一下：感觉还不是很能理解Navigation的作用以及解决的痛点问题，先放一下，太费时间

--- app/src/main/java/com/example/wanandroidjetpack/MainActivity.kt
+++ app/src/main/java/com/example/wanandroidjetpack/MainActivity.kt
@@ -1,27 +1,64 @@
 package com.example.wanandroidjetpack
 
 import android.os.Bundle
-import androidx.navigation.Navigation
-import androidx.navigation.ui.NavigationUI.onNavDestinationSelected
-import androidx.navigation.ui.setupWithNavController
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.Observer
+import androidx.navigation.NavController
+import androidx.navigation.ui.setupActionBarWithNavController
 import com.example.wanandroidjetpack.base.BaseActivity
+import com.example.wanandroidjetpack.base.setupWithNavController
 import com.example.wanandroidjetpack.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : BaseActivity<ActivityMainBinding>() {
+
+    private var currentNavController: LiveData<NavController>? = null
+
     override fun getLayouId(): Int {
         return R.layout.activity_main
     }
 
     override fun initActivity(savedInstanceState: Bundle?) {
-
-        mBinding.navigationView.setupWithNavController(Navigation.findNavController(this,R.id.nav_host_fragment))
-
-        //记得menu中item的id和navigation.xml中fragment的id要相同，要不然无法跳转
-        mBinding.navigationView.setOnNavigationItemSelectedListener {
-            item->
-            onNavDestinationSelected(item, Navigation.findNavController(this, R.id.nav_host_fragment))
+        if (savedInstanceState == null) {
+            setupBottomNavigationBar()
         }
+    }
 
+    /**
+     * Called on first creation and when restoring state.
+     */
+    private fun setupBottomNavigationBar() {
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.navigation_view)
+
+        val navGraphIds = listOf(R.navigation.home_navigation, R.navigation.public_navigation,
+            R.navigation.project_navigation,R.navigation.square_navigation,R.navigation.system_navigation)
+
+        // Setup the bottom navigation view with a list of navigation graphs
+        val controller = bottomNavigationView.setupWithNavController(
+            navGraphIds = navGraphIds,
+            fragmentManager = supportFragmentManager,
+            containerId = R.id.nav_host_fragment,
+            intent = intent
+        )
+
+        // Whenever the selected controller changes, setup the action bar.
+//        controller.observe(this, Observer { navController ->
+//            setupActionBarWithNavController(navController)
+//        })
+
+        currentNavController = controller
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return currentNavController?.value?.navigateUp() ?: false
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle?) {
+        super.onRestoreInstanceState(savedInstanceState)
+        // Now that BottomNavigationBar has restored its instance state
+        // and its selectedItemId, we can proceed with setting up the
+        // BottomNavigationBar with Navigation
+        setupBottomNavigationBar()
     }
 
 
---
+++ app/src/main/java/com/example/wanandroidjetpack/MainActivity2.kt
@@ -0,0 +1,29 @@
+package com.example.wanandroidjetpack
+
+import android.os.Bundle
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI.onNavDestinationSelected
+import androidx.navigation.ui.setupWithNavController
+import com.example.wanandroidjetpack.base.BaseActivity
+import com.example.wanandroidjetpack.databinding.ActivityMainBinding
+
+class MainActivity2 : BaseActivity<ActivityMainBinding>() {
+    override fun getLayouId(): Int {
+        return R.layout.activity_main
+    }
+
+    override fun initActivity(savedInstanceState: Bundle?) {
+
+        mBinding.navigationView.setupWithNavController(Navigation.findNavController(this,R.id.nav_host_fragment))
+
+        //记得menu中item的id和navigation.xml中fragment的id要相同，要不然无法跳转
+        mBinding.navigationView.setOnNavigationItemSelectedListener {
+            item->
+            onNavDestinationSelected(item, Navigation.findNavController(this, R.id.nav_host_fragment))
+        }
+
+    }
+
+
+
+}
